ceda269c7c54749ba06b904a6cf22bc161d28fa9
javabuk <joprieto@gmail.com>
2015-09-03 21:34:52 +0200

    Mostrar información más detallada en el interceptor:
    -Nombre del usuario
    -Nombre de la clase
    -Nombre del método.
    -Parámetros.
    -Valor devuelto.

--- PruebasJEE7Septiembre/src/main/java/com/airhacks/HolaInterceptor.java
+++ PruebasJEE7Septiembre/src/main/java/com/airhacks/HolaInterceptor.java
@@ -1,22 +1,38 @@
 package com.airhacks;
 
+import javax.annotation.Resource;
+import javax.ejb.SessionContext;
 import javax.interceptor.AroundInvoke;
 import javax.interceptor.InvocationContext;
+import javax.servlet.SessionCookieConfig;
 
 public class HolaInterceptor {
 
+	@Resource
+	private SessionContext sessionCtx;
+	
 	@AroundInvoke
 	public Object modifyGreeting(InvocationContext ctx) throws Exception {
-	    Object[] parameters = ctx.getParameters();
+	    //Usuario
+		System.out.print(sessionCtx.getCallerPrincipal().getName() + "-");
+		//Nombre de la clase
+		System.out.print(ctx.getTarget().getClass().getName() + " ");
+		//Nombre del método
+		System.out.print(ctx.getMethod().getName());
+		
+		Object[] parameters = ctx.getParameters();
+	    
 	    for (int i = 0; i < parameters.length; i++) {
-			System.out.println("parametros: " + parameters[i]);
+			System.out.print(" parametros: " + parameters[i]);
 		}
 //	    String param = (String) parameters[0];
 //	    param = param.toLowerCase();
 //	    parameters[0] = param;
 //	    ctx.setParameters(parameters);
 	    try {
-	        return ctx.proceed();
+	        Object objetoDevuelto = ctx.proceed();
+	    	System.out.println(" Valor devuelto:  " + objetoDevuelto.toString());
+	    	return objetoDevuelto;
 	    } catch (Exception e) {
 	        System.out.println("Error calling ctx.proceed in modifyGreeting()");
 	        return null;
